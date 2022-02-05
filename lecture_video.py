ac1c7d4ad8b57154728868a5bafc725b48c2b74c
ArthurMynl <61516506+ArthurMynl@users.noreply.github.com>
2022-02-05 23:11:10 +0100

    adding tqdm for processing and the requirements

--- lecture_video.py
+++ lecture_video.py
@@ -1,33 +1,42 @@
 import cv2
 import face_recognition 
+from tqdm import tqdm 
 
-# preprocessing
-video_lecture = cv2.VideoCapture('video.mp4')
-check , vid = video_lecture.read()
+# getting the path of the video to display
+with open('config.txt', 'r') as f:
+    path = f.readline().split(" ")[-1]
 
+
+# initialization of few variables
 counter = 0
 check = True
 frame_list = []
-
-while(check == True):
-    cv2.imwrite("frame%d.jpg" %counter , vid)
-    check , vid = video_lecture.read()
-    frame_list.append(vid)
-    counter += 1
-    print(counter)
-    
-frame_list.pop()
-
-
 face_locations = []
 process_this_frame = True
 cpt = 1
 reverse, speed = False, False
 
+# preprocessing of the video
+video_lecture = cv2.VideoCapture(path)
+check , vid = video_lecture.read()
+frame_count = int(video_lecture.get(cv2.CAP_PROP_FRAME_COUNT))
+
+for i in tqdm(range(frame_count), desc="Preprocessing"):
+    if check == True:
+        cv2.imwrite("frame%d.jpg" %counter , vid)
+        check , vid = video_lecture.read()
+        frame_list.append(vid)
+        counter += 1
+    
+frame_list.pop()
+
+
+
+
 video_capture = cv2.VideoCapture(0)
 
 while(True):
-    # calcul du nombre de personnes
+    # calculation of the number of persons before the screen
     ret, frame = video_capture.read()
     small_frame = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5)
     rgb_small_frame = small_frame[:, :, ::-1]
@@ -36,9 +45,7 @@ while(True):
         nb_faces = len(face_locations)
     process_this_frame = not process_this_frame
     
-    # debug
-    print(nb_faces)
-
+    # configuration of the variables to play the video
     if(nb_faces > 5):
         reverse = True
         speed = False
@@ -49,7 +56,7 @@ while(True):
         reverse = False
         speed = False
 
-    # gestion du défilement des images
+    # incrementation or decrementation of the counter to display the video
     if(speed):
         cpt += 2
     elif(reverse):
@@ -64,6 +71,7 @@ while(True):
     
     cv2.imshow('video', frame_list[cpt])
 
+    # q or ctrl + c to stop the program
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
